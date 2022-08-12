17849e7b4f0a74b6cd77a1790f31be95d8655be0
Luan Pinto <luan.nelmo@hotmail.com>
2022-08-12 19:42:36 -0300

    ProdutosController: PUT

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -49,5 +49,19 @@ namespace ApiCatalogo.Controllers
 
             return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
         }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Produto produto)
+        {
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
     }
 }
