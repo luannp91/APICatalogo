f863aa37477082c18eadfd8b5c9c8de5001dd891
Luan Pinto <luan.nelmo@hotmail.com>
2022-08-12 19:33:50 -0300

    ProdutosController: POST

--- Controllers/ProdutosController.cs
+++ Controllers/ProdutosController.cs
@@ -26,7 +26,7 @@ namespace ApiCatalogo.Controllers
             return produtos;
         }
 
-        [HttpGet("{id:int}")]
+        [HttpGet("{id:int}", Name ="ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
             var produto = _context.Produtos?.FirstOrDefault(p => p.ProdutoId == id);
@@ -35,6 +35,19 @@ namespace ApiCatalogo.Controllers
                 return NotFound("Produto não encontrados :(");
             }
             return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post(Produto produto)
+        {
+            if (produto is null)
+            {
+                return BadRequest();
+            }
+            _context.Produtos?.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
         }
     }
 }
