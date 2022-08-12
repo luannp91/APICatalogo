8a695f4a7ca34faacbdecda3b71d4121d5bec787
Luan Pinto <luan.nelmo@hotmail.com>
2022-08-12 19:23:15 -0300

    ProdutosController: GET

---
+++ Controllers/ProdutosController.cs
@@ -0,0 +1,40 @@
+﻿using ApiCatalogo.Context;
+using ApiCatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos?.ToList();
+            if (produtos is null)
+            {
+                return NotFound("Produtos não encontrados :(");
+            }
+            return produtos;
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos?.FirstOrDefault(p => p.ProdutoId == id);
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrados :(");
+            }
+            return produto;
+        }
+    }
+}
